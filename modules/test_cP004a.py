ce072753f6b8e36ee903994704b4f8bb4db317e8
María Fernanda Umbarila Suárez <fernandaumbarila@gmail.com>
2023-04-16 15:27:10 -0500

    Cuarta modificación, arreglados en total 6 CP

--- modules/test_cP004a.py
+++ modules/test_cP004a.py
@@ -8,6 +8,7 @@ from selenium import webdriver
 from selenium.webdriver.common.by import By
 from selenium.common.exceptions import *
 import pandas as pd
+import os, csv
 
 #Cambio de nombre de usuario por uno no registrado previamente
 class TestCP004a():
@@ -16,59 +17,96 @@ class TestCP004a():
     self.driver = webdriver.Chrome()
     self.vars = {}
   
-  def teardown_method(self):
-    self.driver.quit()
-  
   def test_cP004a(self):
     
-    data = pd.read_csv("values/values_cP004a.csv")
+    directorio = os.path.abspath("./values/values_cP004a.csv").replace('\\', '/')
+    data = pd.read_csv(directorio)
     contador = 0
     log = []
+    url = "https://tucan.toolsincloud.net/"
 
     #Para cada dato en el archivo CSV
     for e, p, u in zip(data['email'], data['password'], data['username']):
 
       contador += 1
+        
+      self.driver.get(url)
+      #Maximizar ventana
+      self.driver.maximize_window()
+
+      #Valores de la columna email
+      self.driver.find_element(By.NAME, "email").click()        
+      self.driver.find_element(By.NAME, "email").send_keys(e)
+
+      #Valores de la columna password
+      self.driver.find_element(By.NAME, "password").click()
+      self.driver.find_element(By.NAME, "password").send_keys(p)
+      self.driver.find_element(By.NAME, "login").click()
 
       try:
-        
-        self.driver.get("https://tucan.toolsincloud.net/home.php")
-        #Maximizar ventana
-        self.driver.maximize_window()
-        self.driver.find_element(By.NAME, "email").click()
-        #Valores de la columna email
-        self.driver.find_element(By.NAME, "email").send_keys(e)
-        self.driver.find_element(By.NAME, "password").click()
-        #Valores de la columna password
-        self.driver.find_element(By.NAME, "password").send_keys(p)
-        self.driver.find_element(By.NAME, "login").click()
+
         #Esperar para que el elemento siguiente aparezca en pantalla
         self.driver.implicitly_wait(2)
         self.driver.find_element(By.CSS_SELECTOR, ".grid-sidebar:nth-child(9) img").click()
         self.driver.find_element(By.ID, "exampleInputPassword1").click()
+
         #Limpiar el campo
         self.driver.find_element(By.ID, "exampleInputPassword1").clear()
         #Valores de la columna username
         self.driver.find_element(By.ID, "exampleInputPassword1").send_keys(u)
+
         self.driver.find_element(By.NAME, "submit").click()
+
+        if self.driver.find_element(By.CLASS_NAME, "username").text == f'@{u}':
+
+          #Log para el caso fallido
+          log.append([
+            contador,
+            'Exitosa', 
+            'Nombre de usuario ha sido modificado', 
+            f'{e}-{p}-{u}']
+          )  
+          
+        else:
+
+          #Log para el caso fallido
+          log.append([
+            contador,
+            'Fallida', 
+            'Nombre de usuario no fue cambiado', 
+            f'{e}-{p}-{u}']
+          )
+
         #Esperar para que el elemento siguiente aparezca en pantalla
         self.driver.implicitly_wait(2)
         self.driver.find_element(By.CSS_SELECTOR, ".fa-sign-out-alt").click()
-        #Esperar para poder ver el resultado
-        self.driver.implicitly_wait(2)
+      
+      except NoSuchElementException:  
 
-        #Si todo salió bien, agrega el log de éxito
-        log.append([contador, 'Ejecución exitosa', f'{e}-{p}-{u}'])
+        #Log para el caso fallido
+        log.append([
+          contador,
+          'Fallida', 
+          'Login de usuario fallido', 
+          f'{e}-{p}']
+        )
 
-      #Si se presenta alguna excepción también se guarda
-      except NoSuchElementException:
-        log.append([contador, 'No se encontró el elemento dentro de la página', f'{e}-{p}-{u}'])
-        return log
-      except ElementNotInteractableException:
-        log.append([contador, 'No se puede interactuar con el elemento', f'{e}-{p}-{u}'])
-        return log
-      except AttributeError:
-        log.append([contador, 'Otro error ocurrió', f'{e}-{p}-{u}'])
-        return log
+        continue      
   
     return log
+
+#Escritura de los resultados en CSV
+  def escribirResultado():
+
+    nuevaInstancia = TestCP004a()
+    nuevaInstancia.setup_method()
+    ejecucion = nuevaInstancia.test_cP004a()
+    print(ejecucion)
+
+    with open('./results/result_cp004a.csv', 'w+', newline='') as file:
+      
+      writer = csv.writer(file)
+      writer.writerow(['Ejecucion', 'Resultado', 'Detalle', 'DatosUsados'])
+
+      for i in ejecucion:
+        writer.writerow([i[0], i[1], i[2], i[3]])
--- modules/test_cP004b.py
+++ modules/test_cP004b.py
@@ -8,6 +8,7 @@ from selenium import webdriver
 from selenium.webdriver.common.by import By
 from selenium.common.exceptions import *
 import pandas as pd
+import os, csv
 
 #Cambio de nombre de usuario por uno registrado previamente
 class TestCP004b():
@@ -16,61 +17,99 @@ class TestCP004b():
     self.driver = webdriver.Chrome()
     self.vars = {}
   
-  def teardown_method(self):
-    self.driver.quit()
-  
   def test_cP004b(self):
     
-    data = pd.read_csv("values/values_cP004b.csv")
+    directorio = os.path.abspath("./values/values_cP004b.csv").replace('\\', '/')
+    data = pd.read_csv(directorio)
     contador = 0
     log = []
+    url = "https://tucan.toolsincloud.net/"
 
     #Para cada dato en el archivo CSV
     for e, p, u in zip(data['email'], data['password'], data['username']):
 
       contador += 1
+        
+      self.driver.get(url)
+      #Maximizar ventana
+      self.driver.maximize_window()
+
+      #Valores de la columna email
+      self.driver.find_element(By.NAME, "email").click()        
+      self.driver.find_element(By.NAME, "email").send_keys(e)
+
+      #Valores de la columna password
+      self.driver.find_element(By.NAME, "password").click()
+      self.driver.find_element(By.NAME, "password").send_keys(p)
+      self.driver.find_element(By.NAME, "login").click()
 
       try:
 
-        self.driver.get("https://tucan.toolsincloud.net/home.php")
-        #Maximizar ventana
-        self.driver.maximize_window()
-        self.driver.find_element(By.NAME, "email").click()
-        #Valores de la columna email
-        self.driver.find_element(By.NAME, "email").send_keys(e)
-        self.driver.find_element(By.NAME, "password").click()
-        #Valores de la columna password
-        self.driver.find_element(By.NAME, "password").send_keys(p)
-        self.driver.find_element(By.NAME, "login").click()
         #Esperar para que el elemento siguiente aparezca en pantalla
-        self.driver.implicitly_wait(10)
+        self.driver.implicitly_wait(2)
         self.driver.find_element(By.CSS_SELECTOR, ".grid-sidebar:nth-child(9) img").click()
         self.driver.find_element(By.ID, "exampleInputPassword1").click()
+
         #Limpiar el campo
         self.driver.find_element(By.ID, "exampleInputPassword1").clear()
         #Valores de la columna username
         self.driver.find_element(By.ID, "exampleInputPassword1").send_keys(u)
+
         self.driver.find_element(By.NAME, "submit").click()
+
+        try:
+
+          #Busca el mensaje de error
+          self.driver.find_element(By.XPATH, "//p[contains(text(), ' This username is already use ')]")
+
+          #Log para el caso fallido
+          log.append([
+            contador,
+            'Exitosa', 
+            'Nombre de usuario no ha sido modificado', 
+            f'{e}-{p}-{u}']
+          )  
+
+        except NoSuchElementException:
+
+          #Log para el caso fallido
+          log.append([
+            contador,
+            'Fallida', 
+            'Nombre de usuario fue cambiado aunque ya exista', 
+            f'{e}-{p}-{u}']
+          )
+
         #Esperar para que el elemento siguiente aparezca en pantalla
         self.driver.implicitly_wait(2)
         self.driver.find_element(By.CSS_SELECTOR, ".fa-sign-out-alt").click()
-        #Esperar para poder ver el resultado
-        self.driver.implicitly_wait(2)
+      
+      except NoSuchElementException:  
 
-        #Si todo salió bien, agrega el log de éxito
-        log.append([contador, 'Ejecución exitosa', f'{e}-{p}-{u}'])
+        #Log para el caso fallido
+        log.append([
+          contador,
+          'Fallida', 
+          'Login de usuario fallido', 
+          f'{e}-{p}']
+        )
 
-      #Si se presenta alguna excepción también se guarda
-      except NoSuchElementException:
-        log.append([contador, 'No se encontró el elemento dentro de la página', f'{e}-{p}-{u}'])
-        return log
-      except ElementNotInteractableException:
-        log.append([contador, 'No se puede interactuar con el elemento', f'{e}-{p}-{u}'])
-        return log
-      except AttributeError:
-        log.append([contador, 'Otro error ocurrió', f'{e}-{p}-{u}'])
-        return log
+        continue      
   
     return log
 
-     
+#Escritura de los resultados en CSV
+  def escribirResultado():
+
+    nuevaInstancia = TestCP004b()
+    nuevaInstancia.setup_method()
+    ejecucion = nuevaInstancia.test_cP004b()
+    print(ejecucion)
+
+    with open('./results/result_cp004b.csv', 'w+', newline='') as file:
+      
+      writer = csv.writer(file)
+      writer.writerow(['Ejecucion', 'Resultado', 'Detalle', 'DatosUsados'])
+
+      for i in ejecucion:
+        writer.writerow([i[0], i[1], i[2], i[3]])
