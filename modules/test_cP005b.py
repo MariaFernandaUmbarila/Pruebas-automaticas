ca0a5e2036e2809d493def7c7681145e71a5e786
María Fernanda Umbarila Suárez <fernandaumbarila@gmail.com>
2023-04-16 16:06:24 -0500

    Sexta modificación, arreglados 10 CP

--- modules/test_cP005b.py
+++ modules/test_cP005b.py
@@ -6,10 +6,10 @@ Ingeniería de Software III
 
 from selenium import webdriver
 from selenium.webdriver.common.by import By
-from selenium.webdriver.common.by import By
 from selenium.common.exceptions import *
 import pandas as pd
-import pyautogui, time
+import os, csv
+import pyautogui
 
 #Cambio de foto de perfil por una foto no válida
 class TestCP005b():
@@ -18,64 +18,98 @@ class TestCP005b():
     self.driver = webdriver.Chrome()
     self.vars = {}
   
-  def teardown_method(self):
-    self.driver.quit()
-  
   def test_cP005b(self):
     
-    data = pd.read_csv("values/values_cP005b.csv")
+    directorio = os.path.abspath("./values/values_cP005b.csv").replace('\\', '/')
+    data = pd.read_csv(directorio)
     contador = 0
     log = []
+    url = "https://tucan.toolsincloud.net/"
 
     #Para cada dato en el archivo CSV
     for e, p, r in zip(data['email'], data['password'], data['route']):
 
       contador += 1
 
+      self.driver.get(url)
+      #Maximizar ventana
+      self.driver.maximize_window()
+
+      #Valores de la columna email
+      self.driver.find_element(By.NAME, "email").click()
+      self.driver.find_element(By.NAME, "email").send_keys(e)
+
+      #Valores de la columna password
+      self.driver.find_element(By.NAME, "password").click()
+      self.driver.find_element(By.NAME, "password").send_keys(p)
+      self.driver.find_element(By.NAME, "login").click()
+      #Esperar para que el elemento siguiente aparezca en pantalla
+      self.driver.implicitly_wait(2) 
+      
       try:
 
-        self.driver.get("https://tucan.toolsincloud.net/home.php")
-        #Maximizar ventana
-        self.driver.maximize_window()
-        self.driver.find_element(By.NAME, "email").click()
-        #Valores de la columna email
-        self.driver.find_element(By.NAME, "email").send_keys(e)
-        self.driver.find_element(By.NAME, "password").click()
-        #Valores de la columna password
-        self.driver.find_element(By.NAME, "password").send_keys(p)
-        self.driver.find_element(By.NAME, "login").click()
-        #Esperar para que el elemento siguiente aparezca en pantalla
-        self.driver.implicitly_wait(2) 
         self.driver.find_element(By.CSS_SELECTOR, ".grid-sidebar:nth-child(7) strong").click()
-        # 4 | click | css=.home-edit-button | 
         self.driver.find_element(By.CSS_SELECTOR, ".home-edit-button").click()
-        # 5 | click | css=.image-upload:nth-child(2) .far | 
         self.driver.find_element(By.CSS_SELECTOR, ".image-upload:nth-child(2) .far").click()
+        
         #Valores de la columna route
         self.driver.find_element(By.ID, "file-input").send_keys(r)
-        #pyautogui.press('enter')
         self.driver.find_element(By.NAME, "update").click()
-        pyautogui.hotkey('esc')
-        time.sleep(3)
+
         pyautogui.hotkey('esc')
         #Esperar para que el elemento siguiente aparezca en pantalla
         self.driver.implicitly_wait(2)
         self.driver.find_element(By.CSS_SELECTOR, ".fa-sign-out-alt").click()
-        #Esperar para poder ver el resultado
-        self.driver.implicitly_wait(2)
 
-        #Si todo salió bien, agrega el log de éxito
-        log.append([contador, 'Ejecución exitosa', f'{e}-{p}-{r}'])
+        try:
 
-      #Si se presenta alguna excepción también se guarda
-      except NoSuchElementException:
-        log.append([contador, 'No se encontró el elemento dentro de la página', f'{e}-{p}-{r}'])
-        return log
-      except ElementNotInteractableException:
-        log.append([contador, 'No se puede interactuar con el elemento', f'{e}-{p}-{r}'])
-        return log
-      except AttributeError:
-        log.append([contador, 'Otro error ocurrió', f'{e}-{p}-{r}'])
-        return log
+          #Verificar que se haya realizado la publicación
+          self.driver.find_element(By.XPATH, "//p[contains(text(), ' image must be image ')]")
+
+          #Log para el caso exitoso
+          log.append([
+            contador,
+            'Exitosa', 
+            'No se realizo el cambio de imagen de perfil', 
+            f'{e}-{p}-{r}']
+          )
+        
+        except NoSuchElementException:  
+
+          #Log para el caso fallido
+          log.append([
+            contador,
+            'Fallida', 
+            'Imagen de usuario fue modificada', 
+            f'{e}-{p}-{r}']
+          )                    
+
+      except NoSuchElementException:  
+
+        #Log para el caso fallido
+        log.append([
+          contador,
+          'Fallida', 
+          'Login de usuario exitoso pero usuario no existe', 
+          f'{e}-{p}']
+        )
+
+        continue
   
     return log
+  
+  #Escritura de los resultados en CSV
+  def escribirResultado():
+
+    nuevaInstancia = TestCP005b()
+    nuevaInstancia.setup_method()
+    ejecucion = nuevaInstancia.test_cP005b()
+    print(ejecucion)
+
+    with open('./results/result_cp005b.csv', 'w+', newline='') as file:
+      
+      writer = csv.writer(file)
+      writer.writerow(['Ejecucion', 'Resultado', 'Detalle', 'DatosUsados'])
+
+      for i in ejecucion:
+        writer.writerow([i[0], i[1], i[2], i[3]])
